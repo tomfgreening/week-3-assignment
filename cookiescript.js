d4b9dd7267c213fd79de3df5bb92a82d8744d63b
Thomas Greening <tomfgreening@gmail.com>
2024-12-20 13:36:32 +0000

    Defining the function to fetch shop upgrade data from API.

--- cookiescript.js
+++ cookiescript.js
@@ -3,11 +3,20 @@ console.log("here we gooooooooo");
 //
 
 let gameData = {
-    cookieCount: 0;
-    cookiesPerSecond: 0;
+  cookieCount: 0,
+  cookiesPerSecond: 0,
 };
 
 // This is me reminding myself that 'const' is used for static values, or values that will not change.
-const upgradesTable = []
+const upgradesTableArray = [];
 // I need to pull (or fetch perhaps) the API data from Joe's API to populate the upgrades shop
 
+const upgradesContainer = document.getElementById("upgradesShop");
+// I;m selecting my DOM element, so I can manipulate the upgrades shop DIV
+async function fetchShopUpgrades () {
+    const result = await fetch(
+        "https://cookie-upgrade-api.vercel.app/api/upgrades"
+    );
+    console.log(response);
+}
+// The purpose of the above function is to fetch the shopupgrade data from the API. The 'await' stop the 'promise' error coming back when running.
