9d83fe6d68e3cdddc6f0173afda9a4fd006d3bd9
Thomas Greening <tomfgreening@gmail.com>
2024-12-20 11:34:11 +0000

    Begining to write JS code. Defining gamedata values and setting up where the upgrades data API will sit.

--- cookiescript.js
+++ cookiescript.js
@@ -1,1 +1,13 @@
 console.log("here we gooooooooo");
+// just reminding myself here that 'let' is used for a value that will change.
+//
+
+let gameData = {
+    cookieCount: 0;
+    cookiesPerSecond: 0;
+};
+
+// This is me reminding myself that 'const' is used for static values, or values that will not change.
+const upgradesTable = []
+// I need to pull (or fetch perhaps) the API data from Joe's API to populate the upgrades shop
+
